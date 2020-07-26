80b266f6a60fc501287c0755493e91b9a1716a9e
Ariefsetiawan900 <arief.setiawan8788@gmail.com>
2020-07-26 17:26:53 +0700

    create effect button scroll to MostPicked section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,13 +8,19 @@ import MostPicked from "../parts/MostPicked";
 import landingPage from "../json/landingPage.json";
 
 export default class LandingPage extends Component {
+  constructor(props){
+    super(props)
+    this.refMostPicked = React.createRef()
+  }
   render() {
     console.log(this.props);
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero} />
-        <MostPicked data={landingPage.mostPicked} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+        refMostPicked={this.refMostPicked}
+        data={landingPage.mostPicked} />
       </>
     );
   }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -4,12 +4,13 @@ import Button from "../elements/Button";
 
 export default function MostPicked(props) {
   return (
-    <section className="container">
+    <section className="container" ref={props.refMostPicked} style={{height:2000}}>
       <h4 className="mb-3">Most Picked</h4>
       <div className="container-grid">
         {props.data.map((item, index) => {
           return (
             <div
+              key={`mostpicked-${index}`}
               className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
             >
               <div className="card card-featured">
