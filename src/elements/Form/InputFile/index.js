d338b86c34fbc0f9d0a50f41b502437c462aa326
ariefsetiawan900 <arief.setiawan8788@gmail.com>
2020-08-08 21:46:09 +0700

    create & styling element/form/InputFile

---
+++ src/elements/Form/InputFile/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import propTypes from "prop-types";
+import { useRef } from "react";
+
+export default function File(props) {
+  const {
+    value,
+    placeHolder,
+    name,
+    accept,
+    prepend,
+    append,
+    outerClassName,
+    inputClassName,
+  } = props;
+
+  const refInputFile = useRef(null);
+
+  return (
+    <div className={["input-text mb-3", outerClassName].join(" ")}>
+      <div className="input-group">
+        {prepend && (
+          <div className="input-group-prepend bg-gray-900">
+            <span className="input-group-text">{prepend}</span>
+          </div>
+        )}
+        <input
+          accept={accept}
+          ref={refInputFile}
+          name={name}
+          className="d-none"
+          type="file"
+          value={value}
+          onChange={props.onChange}
+        />
+        <input
+          onClick={() => refInputFile.current.click()}
+          defaultValue={value}
+          placeHolder={placeHolder}
+          className={["form-control", inputClassName].join(" ")}
+        />
+        {append && (
+          <div className="input-group-append bg-gray-900">
+            <span className="input-group-text">{append}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+File.defaultProps = {
+  placeHolder: "Browse a file...",
+};
+
+File.propTypes = {
+  name: propTypes.string.isRequired,
+  accept: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
+  prepend: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  append: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  placeHolder: propTypes.string,
+  outerClassName: propTypes.string,
+  inputClassName: propTypes.string,
+};
